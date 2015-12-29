8752b10ec1dbe367ab43a5feaeb7a0a2a754f943
Mark Bain <markbaindesign@gmail.com>
2015-12-29 18:41:50 +0100

    comment out all plugins in load.php

--- root/httpdocs/wp-content/mu-plugins/load.php
+++ root/httpdocs/wp-content/mu-plugins/load.php
@@ -3,13 +3,12 @@
 Plugin Name: Must-use Plugin Loader
 Description: This is just a simple helper plugin which loads other plugins located in /wp-content/mu-plugins, including a useful collection of development tools.
 Author: Mark Bain
-Version: 1.7
+Version: 1.8
 Author URI: http://markbaindesign.com
 */
 
-// mu-plugins/load.php
 
-// Dev
+
 // require WPMU_PLUGIN_DIR.'/dev/ajax-thumbnail-rebuild/ajax-thumbnail-rebuild.php';
 // require WPMU_PLUGIN_DIR.'/dev/auto-post-thumbnail/auto-post-thumbnail.php';
 // require WPMU_PLUGIN_DIR.'/dev/random-post-thumbnail/random-post-thumbnail.php';
@@ -21,16 +20,16 @@ Author URI: http://markbaindesign.com
 	// require WPMU_PLUGIN_DIR.'/dev/debug-wp-query/debug-wp-query.php';
 
 	/* Query Monitor */
-	require WPMU_PLUGIN_DIR.'/dev/query-monitor/query-monitor.php';
+	// require WPMU_PLUGIN_DIR.'/dev/query-monitor/query-monitor.php';
 
 	/* Regenerate Thumbnails */
-	require WPMU_PLUGIN_DIR.'/dev/regenerate-thumbnails/regenerate-thumbnails.php';	
+	// require WPMU_PLUGIN_DIR.'/dev/regenerate-thumbnails/regenerate-thumbnails.php';	
 
 	/* Simply Show IDs */
-	require WPMU_PLUGIN_DIR.'/dev/simply-show-ids/simply-show-ids.php';
+	// require WPMU_PLUGIN_DIR.'/dev/simply-show-ids/simply-show-ids.php';
 
 	/* Pig Latin */
-		// require WPMU_PLUGIN_DIR.'/dev/piglatin/piglatin.php';	
+	// require WPMU_PLUGIN_DIR.'/dev/piglatin/piglatin.php';	
 
 	/* Show Current Template */
-	require WPMU_PLUGIN_DIR.'/dev/show-current-template/show-current-template.php';	
+	// require WPMU_PLUGIN_DIR.'/dev/show-current-template/show-current-template.php';	
